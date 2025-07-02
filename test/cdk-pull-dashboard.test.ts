7ebce1e1782345b9a40d04e5e839255911073cb4
Your Name <you@example.com>
2025-07-02 21:48:52 +0530

    Initial commit

---
+++ test/cdk-pull-dashboard.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkPullDashboard from '../lib/cdk-pull-dashboard-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-pull-dashboard-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkPullDashboard.CdkPullDashboardStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
